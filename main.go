fec1af6de165ed486e6db9e165c4991d35a289e9
Geoffrey Gane <geoffreygane@gmail.com>
2019-08-02 15:56:18 +0200

    test: add sandbox

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+)
 
 func main() {
 	fmt.Println("It all starts here")
+	connection, err := icmp.ListenPacket("udp4", "127.0.0.1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer connection.Close()
+	byteMessage := icmp.Message{
+		Type: ipv4.ICMPTypeEcho,
+		Code: 8,
+		Body: &icmp.Echo{
+			ID:   os.Getpid(),
+			Seq:  1,
+			Data: []byte("hello test"),
+		},
+	}
+
+	message, err := byteMessage.Marshal(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := connection.WriteTo(message, &net.IPAddr{IP: net.ParseIP("127.0.0.1"), Zone: "en0"}); err != nil {
+		log.Fatal(err)
+	}
+
+	rb := make([]byte, 1500)
+	n, _, err := connection.ReadFrom(rb)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rm, err := icmp.ParseMessage(1, rb[:n])
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("got %+v", rm)
+
 }
